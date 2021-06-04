3eba4aab7f28bf74db1d156bdc8f920e1101f66b
NIASKA <mkman751@yahoo.com>
2021-06-03 23:16:52 -0500

    added basic files structure and other basic js files

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes.js');
+const dashboardRoutes = require('./dashboardRoutes.js');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+router.use('./dashboard', dashboardRoutes);
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
