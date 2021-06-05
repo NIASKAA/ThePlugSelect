272828ac0081e9d1b75a92445f43e91bfd855ffe
Thomas Bolling <Thomas.Bolling.87@gmail.com>
2021-06-05 11:58:21 -0500

    updated the category & product models

--- models/Product.js
+++ models/Product.js
@@ -6,6 +6,42 @@ class Product extends Model {}
 
 Product.init(
     {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+        },
+        stock: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'category',
+                key: 'id',
+                unique: false
+            }
+        },
+        brand_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'brand',
+                key: 'id',
+                unique: false
+            }
+        }
+    },
+    {
         sequelize,
         timestamps: false,
         freezeTableName: true,
