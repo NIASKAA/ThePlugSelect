4f00b5e98651aaf60c3193e58240db7805a71c3a
NIASKA <mkman751@yahoo.com>
2021-06-11 14:23:14 -0500

    fixed login video page

--- models/User.js
+++ models/User.js
@@ -19,6 +19,9 @@ User.init(
         username: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                notEmpty: true,
+            }
         },
         email: {
             type: DataTypes.STRING,
