e5097b5e67d5e8436c263f41e4d2437084eacf10
Gonzalez Guerrero <smg061@shsu.edu>
2021-06-13 20:16:48 -0500

    routed profileRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,14 +1,13 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-
-const productRoutes = require('./productRoutes');
-const categoryRoutes = require('./categoryRoutes');
-const tagRoutes = require('./tagRoutes');
-const userRoutes = require('./userRoutes');
-const uploadRoutes = require('./imageUploadRoutes')
-router.use('/products', productRoutes);
-router.use('/categories', categoryRoutes);
-router.use('/tags', tagRoutes);
-router.use('/users', userRoutes);
-router.use('/upload', uploadRoutes)
+const productRoutes = require("./productRoutes");
+const categoryRoutes = require("./categoryRoutes");
+const tagRoutes = require("./tagRoutes");
+const userRoutes = require("./userRoutes");
+const uploadRoutes = require("./imageUploadRoutes");
+router.use("/products", productRoutes);
+router.use("/categories", categoryRoutes);
+router.use("/tags", tagRoutes);
+router.use("/users", userRoutes);
+router.use("/upload", uploadRoutes);
 module.exports = router;
