25e42f9eb577be4303ef929f8063fbcb556d8e1f
Thomas Bolling <Thomas.Bolling.87@gmail.com>
2021-06-14 23:57:14 -0500

    Added bid timer

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const bodyParser = require("body-parser");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Timer for auction dependencies
-Timer = require('./public/js/timer').Timer,
-timer = new Timer();
+// Timer = require('./public/js/timer').Timer,
+// timer = new Timer();
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -45,59 +45,6 @@ app.use(express.static("images"));
 app.use(express.static("video"));
 app.use(routes);
 
-<<<<<<< HEAD
-const botName = "Plug Bot";
-
-
-io.on('connection', socket => {
-    console.log('New WS Connection...');
-
-//     socket.on('joinRoom', ({ username, bidRoom }) => {
-//         const user = userJoin(socket.id, username, bidRoom)
-
-//         socket.join(user.bidRoom);
-
-//         // Welcome message to user
-//         socket.emit('message', 'Welcome to The Plug Select');
-
-//         // Broadcast when a user connects
-//         socket.broadcast.to(user.bidRoom).emit(
-//             'message', 
-//             formatMessage(botName, `${user.username} has joined the chat`)
-//         );
-
-//         // Sends users to room
-//         io.to(user.bidRoom).emit('roomUsers', {
-//             room: userbidRoom,
-//             users: getRoomUsers(user.bidRoom)
-//         });
-//     });
-
-//     // Runs when client disconnects
-//     socket.on('disconnect', () => {
-//         const user = userLeave(socket.id);
-
-//         if (user) {
-//             io.to(user.bidRoom).emit(
-//                 'message',
-//                 formatMessage(botName, `${user.username} has left the chat`)
-//             );
-//         }
-//     });
-
-//     Listen for chatMessage
-//     socket.on('chatMessage', msg => {
-//         const user = getCurrentUser(socket.id);
-
-//         io.to(user.bidRoom).emit('message', formatMessage(user.username,msg));
-//     });
-// });
-// app.use(
-//    fileUpload({
-//       createParentPath: true,
-//    })
-});
-=======
 const numUsers = 0;
 
 io.on('connection', socket => {
@@ -121,7 +68,6 @@ app.use(
       createParentPath: true,
    })
 );
->>>>>>> master
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
