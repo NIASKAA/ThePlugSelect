8342a176be26fd54f2e0844c8cf3233335d2abb3
Gonzalez Guerrero <smg061@shsu.edu>
2021-06-14 19:42:54 -0500

    some changes to socket messages, some style changes on chat

--- public/js/chatbox.js
+++ public/js/chatbox.js
@@ -3,7 +3,7 @@ const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
-const username = document.getElementById('userName');
+const username = document.querySelector('.userName').textContent.trim();
 const chatWindow = document.querySelector('.chatContainer')
 
 const socket = io();
@@ -17,8 +17,7 @@ const renderMessage = message => {
   
   
 socket.on('message', (data) => {
-    document.getElementById('test').innerHTML = data;
-
+    document.getElementById('test').innerHTML = `${username} joined the chat`;
 })
 
 const sendMessage = () => {
@@ -26,10 +25,10 @@ const sendMessage = () => {
 }
 
 socket.on('chat', message => {
-  console.log('From server: ', message)
+  console.log(`${username} ${message}`)
 })
 
-socket.on('chat', message=> {
+socket.on('chat', message => {
     renderMessage(message)
 })
   
