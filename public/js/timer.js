909a68cddcc7d413057037dfed8d76e9873405b7
Santos <max.go.95@gmail.com>
2021-07-24 19:56:01 -0500

    added timer and ability to post items with bid start and bid end date

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,57 +1,69 @@
 // Bid timer setup
-
 let chatBox = document.querySelector(".msgBody");
 let bidButtons = document.querySelector(".cardEnd");
 let timer = document.getElementById("bid-timer").innerHTML;
+const endTime = document.querySelector("#endTime").dataset.endtime;
 
+console.log(endTime);
 function hideButtons() {
-  let buttons = document.querySelectorAll("button");
-  buttons.forEach((button) => button.setAttribute("style", "display:none"));
+   let buttons = document.querySelectorAll("button");
+   buttons.forEach((button) => button.setAttribute("style", "display:none"));
 }
 
 function showButtons() {
-  let buttons = document.querySelectorAll("button");
-  buttons.forEach((button) => button.setAttribute("style", "display:"));
+   let buttons = document.querySelectorAll("button");
+   buttons.forEach((button) => button.setAttribute("style", "display:"));
+}
+
+function getTimeRemaining(endtime) {
+   const total = Date.parse(endtime) - Date.parse(new Date());
+   const seconds = Math.floor((total / 1000) % 60);
+   const minutes = Math.floor((total / 1000 / 60) % 60);
+   const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+   const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+   return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds,
+   };
 }
 
 function endAuction() {
-  let auctions = document.querySelectorAll(".render-message");
-  let winnerBid = auctions[auctions.length - 1].textContent.trim().split(" ");
-  let winnerMessage = `<div class="render-message"> Item sold to ${winnerBid[0]} with a bid of $ ${winnerBid[2]}</div>`;
-  hideButtons();
-  bidButtons.innerHTML = `<h5 class="itemName"> Auction ended </h5>`;
-  chatBox.innerHTML = winnerMessage;
+   let auctions = document.querySelectorAll(".render-message");
+   let winnerBid = auctions[auctions.length - 1].textContent.trim().split(" ");
+   let winnerMessage = `<div class="render-message"> Item sold to ${winnerBid[0]} with a bid of $ ${winnerBid[2]}</div>`;
+   hideButtons();
+   bidButtons.innerHTML = `<h5 class="itemName"> Auction ended </h5>`;
+   chatBox.innerHTML = winnerMessage;
 }
 
-function auctionTimer(num) {
-  var counter = setInterval(function () {
-    document.getElementById("bid-timer").innerHTML = new Date(num * 1000)
-      .toISOString()
-      .substr(11, 8);
-    if (num <= 0) {
-      clearInterval(counter);
-      endAuction();
-    }
-    num--;
-  }, 1000);
+function auctionTimer(deadline) {
+   var counter = setInterval(function () {
+      const t = getTimeRemaining(deadline);
+      const timer = document.getElementById("bid-timer");
+      timer.innerHTML =
+         "days: " +
+         t.days +
+         "<br>" +
+         "hours: " +
+         t.hours +
+         "<br>" +
+         "minutes: " +
+         t.minutes +
+         "<br>" +
+         "seconds: " +
+         t.seconds;
+      if (num <= 0) {
+         clearInterval(counter);
+         endAuction();
+      }
+      num--;
+   }, 1000);
 }
 
-
-function setTimer(num) {
-  hideButtons();
-  var counter = setInterval(function () {
-    document.getElementById("timer").innerHTML = num;
-    if (num <= 0) {
-      clearInterval(counter);
-      showButtons();
-      auctionTimer(30);
-    }
-    num--;
-  }, 1000);
-}
-
-setTimer(10);
-
-
-
-
+showButtons();
+auctionTimer(endTime);
+endTimer();
