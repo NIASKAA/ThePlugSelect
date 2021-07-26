8baa1ff78f48d1e085644de3590daa96c7e41631
Santos <max.go.95@gmail.com>
2021-07-25 22:05:21 -0500

    some style changes and refactorings in the bidItem.js

--- public/js/bidItem.js
+++ public/js/bidItem.js
@@ -1,55 +1,68 @@
 // get the current bidding item id
 let item_id = document.location.href.split("/")[4];
 const userName = document.querySelector(".userName").textContent.trim();
-let currentPrice = Number(
-  document.querySelector("#price").textContent.split(":")[1]
-);
+let currentPrice = Number(document.querySelector("#currentPrice").textContent);
 const chat = document.querySelector(".chat-form");
 
+// this function sets the price's color to red for a few seconds when a bid happens
+
+const setPriceToRed = () => {
+   const price = document.querySelector("#currentPrice");
+   price.setAttribute("class", "increasePrice");
+   setTimeout(function () {
+      price.setAttribute("class", "");
+   }, 2000);
+};
 // request to back end for bidding item
 const bidItem = async (price) => {
-  if (price > currentPrice) {
-    let response = await fetch(`/api/products/${item_id}/bid`, {
-      method: "POST",
-      body: JSON.stringify({ price }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    console.log(response);
-  } else {
-    return;
-  }
+   if (price > currentPrice) {
+      let response = await fetch(`/api/products/${item_id}/bid`, {
+         method: "POST",
+         body: JSON.stringify({ price }),
+         headers: {
+            "Content-Type": "application/json",
+         },
+      });
+      console.log(response);
+   } else {
+      return;
+   }
 };
 
 let buttons = document.querySelectorAll(".bidBtns");
 
 for (let button of buttons) {
-  button.addEventListener("click", function (event) {
-    event.preventDefault();
-    let today = new Date();
-    let time = today.toLocaleTimeString();
-    let bidAmount = Number(event.target.textContent.trim());
-    let message = `${userName} bids ${bidAmount} == ${time}`;
-    if (bidAmount > currentPrice) {
-      socket.emit("chat", message);
-      bidItem(bidAmount);
-    } else {
-      alert("A bid must be greater than the current price");
-    }
-  });
+   button.addEventListener("click", function (event) {
+      event.preventDefault();
+      let today = new Date();
+      let time = today.toLocaleTimeString();
+      let bidAmount = Number(event.target.textContent.trim());
+      let message = `${userName} bids ${bidAmount} == ${time}`;
+      if (bidAmount > currentPrice) {
+         socket.emit("chat", message);
+         bidItem(bidAmount);
+         currentPrice = bidAmount;
+         document.querySelector("#currentPrice").textContent = bidAmount;
+         setPriceToRed();
+      } else {
+         alert("A bid must be greater than the current price");
+      }
+   });
 }
 
 document.querySelector("#customBidBtn").addEventListener("click", (event) => {
-  event.preventDefault();
-  let today = new Date();
-  let time = today.toLocaleTimeString();
-  let customBid = Number(document.querySelector("#bid-form").value.trim());
-  let message = `${userName} bids ${customBid} ==  ${time}`;
-  if (customBid > currentPrice) {
-    socket.emit("chat", message);
-    bidItem(customBid);
-  } else {
-    alert("A bid must be greater than the current price");
-  }
+   event.preventDefault();
+   let today = new Date();
+   let time = today.toLocaleTimeString();
+   let customBid = Number(document.querySelector("#bid-form").value.trim());
+   let message = `${userName} bids ${customBid} ==  ${time}`;
+   if (customBid > currentPrice) {
+      socket.emit("chat", message);
+      bidItem(customBid);
+      currentPrice = customBid;
+      document.querySelector("#currentPrice").textContent = customBid;
+      setPriceToRed();
+   } else {
+      alert("A bid must be greater than the current price");
+   }
 });
--- server.js
+++ server.js
@@ -1,71 +1,74 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const http = require('http');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const socketio = require('socket.io');
+const socketio = require("socket.io");
 const fileUpload = require("express-fileupload");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-// Timer for auction dependencies
-// Timer = require('./public/js/timer').Timer,
-// timer = new Timer();
 
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 
 const PORT = process.env.port || 3001;
 
 const io = socketio(server);
 
 const sess = {
-    secret: 'wetheplugselect',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
+   secret: "wetheplugselect",
+   cookie: {},
+   resave: false,
+   saveUninitialized: true,
+   store: new SequelizeStore({
+      db: sequelize,
+   }),
 };
 
 app.use(session(sess));
-const hbs = exphbs.create({helpers});
+const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use("/public", express.static('./public/'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static("./public/"));
 app.use(express.static("images"));
 app.use(express.static("video"));
 app.use(routes);
 
 const numUsers = 0;
 
-io.on('connection', socket => {
-    const addedUser = false;
-    io.emit('message', '');
-    console.log('User Connected: ' + socket.id);
-    
-    socket.on('message', (data) => {
-        socket.broadcast.emit('message', data);
-    });
+io.on("connection", (socket) => {
+   const addedUser = false;
+   io.emit("message", "");
+   console.log("User Connected: " + socket.id);
 
-    socket.on('chat', message => {
-        io.emit('chat', message)
-    })
-    socket.on('disconnect', function () {
-        socket.broadcast.emit('disconnected');
-  
-    });
+   socket.on("message", (data) => {
+      socket.broadcast.emit("message", data);
+   });
 
+   socket.on("chat", (message) => {
+      io.emit("chat", message);
+   });
+   socket.on("disconnect", function () {
+      socket.broadcast.emit("disconnected");
+   });
+
+   socket.on('join', ({ username, room }, callback) => {
     
-    io.emit('userLeft', "User Disconnected");
-    
+   }); // add user with socket id and room info
+
+   socket.on("say to someone", (id, msg) => {
+      socket.to(id).emit("my message", msg);
+   });
+
+   io.emit("userLeft", "User Disconnected");
 });
 
 app.use(
@@ -74,6 +77,6 @@ app.use(
    })
 );
 
-sequelize.sync({force: false}).then(() => {
-    server.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+sequelize.sync({ force: false }).then(() => {
+   server.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 });
