b94c1701677b97e71dd909d3f8ed640f3aae7012
Thomas Bolling <Thomas.Bolling.87@gmail.com>
2021-07-26 20:10:20 -0500

    cleaned up un-need code

--- public/js/chatbox.js
+++ public/js/chatbox.js
@@ -8,53 +8,6 @@ const input = document.querySelector(".chat-input");
 
 const socket = io();
 
-// const renderMessage = (message) => {
-//   const div = document.createElement("div");
-//   div.classList.add("render-message");
-//   div.innerText = message;
-
-//   chatWindow.appendChild(div);
-// };
-// //socket.connect(`localhost:3000/bid/chat/${item_id}`);
-
-// socket.on("connection", )
-// socket.on("message", (data) => {
-//   document.getElementById("test").innerHTML = data;
-//   message =
-//     username.length > 0
-//       ? `${username} joined the chat`
-//       : "Unregistered user joined the chat";
-//   socket.emit("message", message);
-// });
-
-// const sendMessage = () => {
-//   socket.emit("message", "Hey");
-// };
-
-// socket.on("chat", (message) => {
-//   console.log(`${username} ${message}`);
-// });
-
-// socket.on("disconnect", (message) => {
-//   socket.emit("message", `\n\n\n\n${username} disconnected\n\n\n\n\n`);
-// });
-
-// socket.on("chat", (message) => {
-//   renderMessage(message);
-// });
-
-// socket.on("userLeft", () => {
-//     socket.emit('message', "Hey")
-// })
-
-// Message from server
-// socket.on('message', (message) => {
-//   console.log(message)
-
-//   // Scroll down
-//   chatMessages.scrollTop = chatMessages.scrollHeight;
-// });
-
 const botName = 'Plug Bot';
 
 function userLeave(id) {
@@ -69,7 +22,7 @@ function userLeave(id) {
 chatForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  // Emit message to server
+  // Emit message to display to the chat window
   socket.emit('chatForm', input.value);
   input.value='';
 
@@ -79,7 +32,7 @@ socket.on('chatForm', message => {
   renderMessage(`${chatterName}:  ${message}`)
 })
 
-// Outputs message to the chat window
+// Renders message for the chat window
 const renderMessage = message => {
   const div = document.createElement('div')
   div.classList.add('render-message')
@@ -87,15 +40,3 @@ const renderMessage = message => {
   chatMessages.appendChild(div)
   console.log(chatMessages)
 }
-
-
-
-// // Add users to DOM
-// function outputUsers(users) {
-//   userList.innerHTML = '';
-//   users.forEach((user) => {
-//     const li = document.createElement('li');
-//     li.innerText = user.username;
-//     userList.appendChild(li);
-//   });
-// }
